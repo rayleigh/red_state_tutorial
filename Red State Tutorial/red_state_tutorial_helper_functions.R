0b65747d75432c1373cbf00daea04d81d5a38465
Rayleigh L <rayleighl@yahoo.com>
2015-09-16 17:31:50 -0400

    Updated to incorporate Jonah's suggestions and fix a few things

--- Red State Tutorial/red_state_tutorial_helper_functions.R
+++ Red State Tutorial/red_state_tutorial_helper_functions.R
@@ -25,14 +25,14 @@ extract_results_from_stanfit_object <- function(interested_coefficients, stanfit
 
 #Uses R's basic functions to plot coefficients and their credible intervals
 #based on results from a summarized stanfit object generated from Stan code for a regression model
-plot_coefficients_with_credible_intervals <- function(plot_title, x_labels, coefficient_result_matrix, lower_bound_percent = "2.5%", upper_bound_percent = "97.5%")
+plot_coefficients_with_credible_intervals <- function(plot_title, x_labels, coefficient_result_matrix, lower_bound_percent = "2.5%", upper_bound_percent = "97.5%", plot_rows = row.names(coefficient_result_matrix))
 {
   ##Store the number of interested coefficients for later use
   num_coefficients = length(x_labels)
   
   ##Create the initial plot based on the mean estimates in the stanfit object
-  plot(coefficient_result_matrix[, "mean"],
-       xlim = c(1, num_coefficients + 0.25),
+  plot(coefficient_result_matrix[plot_rows, "mean"],
+       xlim = c(1 - 0.25, num_coefficients + 0.25),
        ylim=range(c(coefficient_result_matrix[, lower_bound_percent], coefficient_result_matrix[, upper_bound_percent])),
        pch=19, xaxt = "n", xlab="Income", ylab="Estimated coefficient",
        main= plot_title
@@ -46,17 +46,17 @@ plot_coefficients_with_credible_intervals <- function(plot_title, x_labels, coef
        cex = 0.9
   )
   ##Add bars
-  arrows(1:num_coefficients, coefficient_result_matrix[, lower_bound_percent], 1:num_coefficients, coefficient_result_matrix[, upper_bound_percent], length=0.05, angle=90, code=3)
+  arrows(1:num_coefficients, coefficient_result_matrix[plot_rows, lower_bound_percent], 1:num_coefficients, coefficient_result_matrix[plot_rows, upper_bound_percent], length=0.05, angle=90, code=3)
 }
 
 #Add a state's coefficients to a plot
-add_state_to_plot <- function(state, state_labels, color, coefficient_result_matrix, lower_bound_percent = "2.5%", upper_bound_percent = "97.5%")
+add_state_to_plot <- function(state, state_labels, color, coefficient_result_matrix, lower_bound_percent = "2.5%", upper_bound_percent = "97.5%", point_offset = 0)
 {
-  points(coefficient_result_matrix[state_labels, "mean"], pch = 6)
-  arrows(1:5, coefficient_result_matrix[state_labels, lower_bound_percent], 
-         1:5, state_and_inc_regression_data[state_labels, upper_bound_percent], 
+  points(1:5 + point_offset, coefficient_result_matrix[state_labels, "mean"], pch = 6)
+  arrows(1:5 + point_offset, coefficient_result_matrix[state_labels, lower_bound_percent], 
+         1:5 + point_offset, state_and_inc_regression_data[state_labels, upper_bound_percent], 
          length=0.05, angle=90, code=3, col = color)
-  text(5, coefficient_result_matrix[state_labels[5], "mean"], labels = c(state), pos = 4)
+  text(5 + point_offset, coefficient_result_matrix[state_labels[5], "mean"], labels = c(state), pos = 4)
   
 }
 
